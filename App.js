a365e05d16bb5072a3b85cac88edcde9fdcf62d1
shinequsar <40741363+shinequasar@users.noreply.github.com>
2020-12-30 18:21:48 +0900

    [Add] start RN project

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{padding: 10, fontSize: 42}}>ToDo list</Text>
+        <Text style={{padding: 10, fontSize: 22}}>일단 투두리스트부터 만들고</Text>
+        <Text style={{padding: 10, fontSize: 22}}>변형해봐야겠다</Text>
+        <Text  style={{padding: 10, fontSize: 15}}>shinequasar</Text>
+        <TextInput
+            style={{height: 40}}
+            placeholder="Type here!"
+            // onChangeText={(text) => this.setState({text})}
+            // value={this.state.text}
+          />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
